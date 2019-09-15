8a03df9d45ff225effd4696b27ca4495b89540bf
Stefan Andrei <stefandrei123@gmail.com>
2019-09-15 12:09:28 +0200

    styling on profile & edit profile -- done

--- edit-profile.php
+++ edit-profile.php
@@ -17,17 +17,26 @@ if (!$_SESSION['jUser']) {
 
 
 <div id="<?= $_SESSION['jUser']->id ?>">
-    <h1>Edit Profile</h1>
-    <h1>Hi <?= $_SESSION['jUser']->name ?>.
-        Your email address is <?= $_SESSION['jUser']->email ?>
-    </h1>
-    <input data-update="name" type="text" value="<?= $_SESSION['jUser']->name ?>">
-    <input data-update="lastName" type="text" value="<?= $_SESSION['jUser']->lastName ?>">
-    <input data-update="email" type="text" maxlength="345" data-type="email" value="<?= $_SESSION['jUser']->email ?>">
-    <input data-update="password" type="text" value="<?= $_SESSION['jUser']->password ?>">
-    <button type="button" id="btnEditUser">Save changes</button>
-    <button onclick="deleteProfile( this )">Delete Profile</button>
+    <div class="">
+
+    </div>
+    <div class="containerUpdate">
+        <label>Update your profile, <?= $_SESSION['jUser']->name ?>.</label>
+        <div class="divUpdate">
+            <label>Name</label>
+            <input data-update="name" type="text" value="<?= $_SESSION['jUser']->name ?>">
+            <label>Last name</label>
+            <input data-update="lastName" type="text" value="<?= $_SESSION['jUser']->lastName ?>">
+            <label>Email</label>
+            <input data-update="email" type="text" maxlength="345" data-type="email" value="<?= $_SESSION['jUser']->email ?>">
+            <label>Password</label>
+            <input data-update="password" type="text" value="<?= $_SESSION['jUser']->password ?>">
+            <button type="button" id="btnEditUser">Save changes</button>
+            <button onclick="deleteProfile( this )">Delete Profile</button>
+        </div>
+    </div>
 </div>
+
 
 <script>
     const USER_ID = '<?= $_SESSION['jUser']->id ?>';
